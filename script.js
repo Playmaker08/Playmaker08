10cb3896bce582c196c82cffc64b5953c4812ba7
Andy Nguyen <nguyenandy1402@gmail.com>
2025-04-13 10:32:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// Business Analyst typed effect for the new element
+var typedRole = new Typed(".typed-role", {
+  strings: ["Business Analyst"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+// Animated line for welcome message
+var typedCodeLine = new Typed(".animated-line", {
+  strings: [
+    'Welcome to the second "home" of the Data-Driven and creativity-based student 👋'
+  ],
+  typeSpeed: 40,
+  showCursor: false,
+  loop: false
+});
